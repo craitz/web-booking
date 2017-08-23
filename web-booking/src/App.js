128a72591bb111661e2a1137933ddc7d1e13e57a
craitz <craitz@gmail.com>
2017-08-23 10:25:38 -0300

    layout

--- web-booking/src/App.js
+++ web-booking/src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
+import AppBar from 'react-toolbox/lib/app_bar/AppBar';
+import Navigation from 'react-toolbox/lib/navigation/Navigation';
+import Link from 'react-toolbox/lib/link/Link';
+import Button from 'react-toolbox/lib/button/Button';
 import logo from './logo.svg';
 import './App.css';
 
+const MainNav = () => (
+  <AppBar
+    title="Web Booking"
+    leftIcon="import_contacts"
+    className="main-nav"
+    scrollHide={true}
+  >
+    <Navigation type="horizontal">
+      <Link href="http://" label='Reservas' icon="bookmark" />
+      <Link href="http://" label="Histórico" icon="history" />
+      <Link href="http://" label="Código-fonte" icon="share" />
+    </Navigation>
+  </AppBar>
+);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDrawerToggle = this.handleDrawerToggle.bind(this);
+    this.handleRequestChange = this.handleRequestChange.bind(this);
+    this.state = {
+      drawerOpen: false
+    }
+  }
+
+  handleDrawerToggle(event) {
+    console.log(event);
+    this.setState({
+      drawerOpen: !this.state.drawerOpen
+    });
+  }
+
+  handleRequestChange(open) {
+    this.setState({
+      drawerOpen: open
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <MainNav />
+        <div className="main-content">
+          <div className="content-header">
+            <span>Olá, seja bem-vindo!</span>
+            <Button
+              icon="code"
+              accent
+              floating
+              className="main-menu-button"
+            />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
